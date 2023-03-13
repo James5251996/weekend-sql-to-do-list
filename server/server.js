a91bb75fef694bd89389cd394b8a39da5e96a04a
Trent Osterman <trentosterman@gmail.com>
2023-03-13 16:41:33 -0500

    changed my port and made my heroku databse.

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 // creating the app call to use express to get the server running
 const app = express();
 const bodyParser = require('body-parser');
-const port = 8000;
+const PORT = process.env.PORT || 8000;
 const router = require('./routes/todolist.router.js')
 
 // setting the body parser to url encoded and static
@@ -14,6 +14,6 @@ app.use(express.static('server/public'));
 app.use('/tasks', router);
 
 // listening fro requests on a port
-app.listen(port, () => {
-    console.log('litening to the port', port);
+app.listen(PORT, () => {
+    console.log('litening to the port', PORT);
 });
